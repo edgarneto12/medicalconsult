072f36652688e4241df971e66cb79fba430cc0ec
edgarneto12 <edgarneto789@gmail.com>
2024-09-09 21:05:40 -0300

    update

--- src/main/java/br/com/edgarneto/medicalconsult/usuario/services/UsuarioService.java
+++ src/main/java/br/com/edgarneto/medicalconsult/usuario/services/UsuarioService.java
@@ -2,7 +2,6 @@ package br.com.edgarneto.medicalconsult.usuario.services;
 
 import br.com.edgarneto.medicalconsult.usuario.models.Usuario;
 import br.com.edgarneto.medicalconsult.usuario.repositories.UsuarioRepository;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.AllArgsConstructor;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,20 +23,24 @@ public class UsuarioService {
     public Usuario cadastrarUsuario(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
+
     public List<Usuario> listarUsuarios() {
         return usuarioRepository.findAll();
     }
+
     public Usuario atualizarUsuario(Usuario usuario) {
         if(usuario.getIdUsuario()==null){
             throw new RuntimeException("Usuario sem ID");
         }
         return usuarioRepository.save(usuario);
     }
+
     public Usuario buscarUsuario(Long id) {
         return usuarioRepository.findById(id).orElseThrow(
                 () -> new ObjectNotFoundException("Usuário não encontrado:", id)
         );
     }
+
     public void excluirUsuario(Long id) {
         usuarioRepository.deleteById(id);
     }
